ba73a665d5335c6dd6703651e6ef4d20315e90ce
Winner <gradiwinner962@gmail.com>
2025-01-06 16:56:02 +0200

    added the main directory

---
+++ app/src/main/java/com/example/articlecompose/MainActivity.kt
@@ -0,0 +1,108 @@
+package com.example.articlecompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextAlign.Companion.Justify
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.articlecompose.ui.theme.ArticleComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ArticleComposeTheme {
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    Column {
+                        WelcomeImage()
+                        TextArticle()
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Composable
+fun WelcomeImage( modifier : Modifier = Modifier) {
+    val image = painterResource(R.drawable.bg_compose_background)
+
+    Box (modifier){
+        Image(
+            painter = image,
+            contentDescription = null,
+
+            alpha = 0.5F
+        )
+    }
+}
+
+@Composable
+fun TextArticle (modifier: Modifier = Modifier) {
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Text(
+            text = stringResource(R.string.article_title),
+            modifier = Modifier.padding(16.dp),
+            fontSize = 24.sp,
+
+
+        )
+
+        Text (
+            text = stringResource(R.string.first_paragraph),
+            modifier = Modifier.padding(16.dp),
+            fontSize = 16.sp,
+            lineHeight = 24.sp,
+            textAlign = Justify
+
+        )
+
+        Text (
+            text = stringResource(R.string.second_paragraph),
+            modifier = Modifier.padding(16.dp),
+            fontSize = 16.sp,
+            lineHeight = 24.sp,
+            textAlign = Justify
+        )
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ArticleComposeTheme {
+        WelcomeImage()
+        TextArticle()
+
+    }
+}
